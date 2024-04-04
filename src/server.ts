f39dd1c1ae024de57e18d74a59c3b4767528f9af
Andrelino Silva <andrelinodev@gmail.com>
2024-04-04 19:06:14 -0300

    first lesson success

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,34 @@
+import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
+import z from "zod";
 
 const app = fastify()
 
-app.get('/', (req, res) => {
-  return 'Hello NLW Unite'
+const prisma = new PrismaClient({
+  log: ['query']
+})
+
+app.post('/events', async (req, replay) => {
+  const createEventSchema = z.object({
+    title: z.string().min(4),
+    details: z.string().nullable(),
+    maximumAttendees: z.number().int().positive().nullable(),
+
+  })
+
+  const data = createEventSchema.parse(req.body)
+
+ const event = await prisma.event.create({
+    data: {
+      title: data.title,
+      details: data.details,
+      maximumAttendees: data.maximumAttendees,
+      slug: new Date().toISOString(),
+    }
+  })
+
+
+  return { eventId: event.id}
 })
 
 app.listen({ port: 3333}).then(() => {
